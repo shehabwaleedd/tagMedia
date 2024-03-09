5867c4e10e7add71544d01a164c14995989069d9
Shehab Waleed <shehabwaleedd@gmail.com>
2024-03-10 01:48:16 +0200

    improvments.

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import styles from "./page.module.scss"
 import ContactUpper from "./components/contactUpper"
 import ContactForm from './components/contactForm'
+import ServicesCo from '@/components/servicesCo'
+
 const page = () => {
     return (
-        <main className={styles.contact}>
-            <ContactUpper />
-            <ContactForm />
-        </main>
+        <>
+            <main className={styles.contact}>
+                <section className={styles.contact_container}>
+                    <ContactUpper />
+                    <ContactForm />
+                </section>
+                <ServicesCo />
+            </main>
+        </>
     )
 }
 
