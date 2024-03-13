2cc03624bad112f3ba01e6e635262ab93a1cc667
Shehab Waleed <shehabwaleedd@gmail.com>
2024-03-14 01:52:15 +0200

    Update styles for aboveFooter and testimonials components

--- src/components/testimonials/index.jsx
+++ src/components/testimonials/index.jsx
@@ -1,17 +1,19 @@
 'use client'
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './style.module.scss';
 import { GoArrowRight } from "react-icons/go";
 import { GoArrowLeft } from "react-icons/go";
-import Magnetic from '@/animation/Magnetic';
 import { Swiper, SwiperSlide } from "swiper/react";
-import useWindowWidth from '@/hooks/useWindowWidth';
 import SwiperCore, { Navigation, Pagination } from 'swiper';
+import Magnetic from '@/animation/Magnetic';
+import useWindowWidth from '@/hooks/useWindowWidth';
 import "swiper/css";
 import "swiper/css/pagination";
 import 'swiper/css/navigation';
 import Image from 'next/image';
 import { CiCirclePlus } from "react-icons/ci";
+import { motion, AnimatePresence } from 'framer-motion';
+
 
 
 SwiperCore.use([Navigation, Pagination]);
@@ -19,29 +21,27 @@ SwiperCore.use([Navigation, Pagination]);
 const testimonialsData = [
     {
         id: 1,
-        img: "/assets/stars/AHMED ZAHER.png",
-        name: "John Doe",
-        position: "Managing Partner",
-        company: "XYZ Company",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam ad harum iure mollitia. Amet corrupti placeat sequi deleniti quisquam quae autem ea quis doloribus sint facilis similique animi a, atque incidunt pariatur impedit quaerat illum ex minima velit, inventore saepe? Nesciunt enim quia, nam illo provident ad velit. Deserunt animi, neque vitae eius voluptate aut doloremque explicabo optio nesciunt totam quisquam et, maxime, molestiae sed quaerat.."
+        img: "/assets/stars/zaher.png",
+        name: "Ahmed Zaher",
+        position: "Actor",
+        company: "Super Start",
+        text: "Working With Tag Media is a great experience, they are very professional and they have a great team. When I met them, I felt that I am in the right place. They are very professional and they have a great team. When I met them, I felt that I am in the right place."
     },
     {
         id: 2,
-        img: "/assets/stars/AHMED ZAHER.png",
-        name: "John Doe",
-        position: "Managing Partner",
-        company: "XYZ Company",
-        title: "Jane Doe, CTO, Another Company",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam ad harum iure mollitia. Amet corrupti placeat sequi deleniti quisquam quae autem ea quis doloribus sint facilis similique animi a, atque incidunt pariatur impedit quaerat illum ex minima velit, inventore saepe? Nesciunt enim quia, nam illo provident ad velit. Deserunt animi, neque vitae eius voluptate aut doloremque explicabo optio nesciunt totam quisquam et, maxime, molestiae sed quaerat."
+        img: "/assets/stars/gelil.jpeg",
+        name: "Amr Abdelgelil",
+        position: "Actor",
+        company: "Super Star",
+        text: "I have been working with Tag Media for a long time, and I have always been satisfied with their work. They are very professional and they have a great team. When I met them, I felt that I am in the right place."
     },
     {
         id: 3,
-        img: "/assets/stars/AHMED ZAHER.png",
-        name: "John Doe",
-        position: "Managing Partner",
-        company: "XYZ Company",
-        title: "Jim Beam, Founder, Startup XYZ",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam ad harum iure mollitia. Amet corrupti placeat sequi deleniti quisquam quae autem ea quis doloribus sint facilis similique animi a, atque incidunt pariatur impedit quaerat illum ex minima velit, inventore saepe? Nesciunt enim quia, nam illo provident ad velit. Deserunt animi, neque vitae eius voluptate aut doloremque explicabo optio nesciunt totam quisquam et, maxime, molestiae sed quaerat."
+        img: "/assets/stars/Mai.png",
+        name: "Mai Omar",
+        position: "Actor",
+        company: "Super Start",
+        text: "It was a pleasure working with Tag Media, they are very professional and they have a great team. When I met them, I felt that I am in the right place. They are very professional and they have a great team. When I met them, I felt that I am in the right place."
     },
 
     {
@@ -78,6 +78,17 @@ const Testimonials = () => {
     const swiperRef = useRef(null);
     const windowWidth = useWindowWidth()
     const isMobile = windowWidth < 777
+    const [detailsOpen, setDetailsOpen] = useState(false);
+    const [selectedTestimonial, setSelectedTestimonial] = useState(null); // New state to track selected testimonial
+
+    const handleDetailsOpen = (testimonial) => {
+        setSelectedTestimonial(testimonial); // Set the selected testimonial
+        setDetailsOpen(true); // Open the details view
+    };
+
+    const handleDetailsClose = () => {
+        setDetailsOpen(false); // Close the details view
+    };
 
     const handleNextSlide = () => {
         if (swiperRef.current) {
@@ -107,7 +118,7 @@ const Testimonials = () => {
                     onSwiper={(swiper) => { swiperRef.current = swiper; }}
                     ref={swiperRef}
                     spaceBetween={isMobile ? 20 : 50}
-                    slidesPerView={isMobile ? 1 : 4}
+                    slidesPerView={isMobile ? 1 : 3}
                     pagination={{ clickable: true }}
                     navigation={
                         {
@@ -125,7 +136,7 @@ const Testimonials = () => {
                                 <p>&quot;{testimonial.text.slice(0, 150)}...&quot;</p>
                                 <div className={styles.testimonials__slide_container_middle}>
                                     <CiCirclePlus />
-                                    <span>
+                                    <span  onClick={() => handleDetailsOpen(testimonial)}  className={styles.testimonials__slide_container_middle_readMore}>
                                         Read More
                                     </span>
                                 </div>
@@ -138,6 +149,24 @@ const Testimonials = () => {
                         </SwiperSlide>
                     ))}
                 </Swiper>
+            <AnimatePresence>
+                {detailsOpen && selectedTestimonial && (
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        className={styles.testimonialsDetailsModal}
+                        onClick={handleDetailsClose}>
+                        <div className={styles.testimonialsDetailsContent}>
+                            <div className={styles.close}> <button onClick={handleDetailsClose}>Close</button> </div>
+                            <Image src={selectedTestimonial.img} alt="testimonial" width={200} height={200} />
+                            <p>{selectedTestimonial.text}</p>
+                            <h4>{selectedTestimonial.name}</h4>
+                            <h5>{selectedTestimonial.position}, {selectedTestimonial.company}</h5>
+                        </div>
+                    </motion.div>
+                )}
+            </AnimatePresence>
             </section>
         </>
     );
